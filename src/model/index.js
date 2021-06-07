785828f6f3ecd922e783f51c236b7d5b7c395722
iNexxon <Dfsgamesteam@gmail.com>
2021-06-08 03:15:32 +0500

    Контроллеры и роуты

---
+++ src/model/index.js
@@ -0,0 +1,16 @@
+const budget = require('./budget');
+const calendar = require('./calendar');
+const chat = require('./chat');
+const concert = require('./concert');
+const locations = require('./locations');
+const notification = require('./notification');
+const repetition = require('./repetition');
+const rulLevel = require('./setList');
+const song = require('./song');
+const songDescription = require('./songDescription');
+const user = require('./user');
+
+
+module.exports = {
+   budget, calendar, chat, concert, locations, notification, repetition, rulLevel, setList, song, songDescription, user,
+}
